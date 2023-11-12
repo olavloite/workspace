06e8a83bd6755d9ededee19d50572049ef7ad134
Knut Olav Løite <koloite@gmail.com>
2023-11-12 07:03:31 +0000

    chore: initial commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,25 @@
 
 package com.mycompany.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.postgresql.Driver");
         System.out.println("Hello Remote World!");
+
+        try (Connection connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/test-db")) {
+          try (ResultSet resultSet = connection
+                .createStatement()
+                .executeQuery(
+                  "select 'The Cloud Spanner Emulator says \"Hello!\" at timestamp ' || current_timestamp::varchar")) {
+            while (resultSet.next()) {
+                System.out.println("Message: " + resultSet.getString(1));
+            }
+          }
+        }
     }
 }
